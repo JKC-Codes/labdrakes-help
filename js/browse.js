ff0592590edc5769dad116c248b3287986d11e98
HalcyonHush <34430187+HalcyonHush@users.noreply.github.com>
2018-01-15 18:48:38 +0000

    Added topic change events

--- js/browse.js
+++ js/browse.js
@@ -1,8 +1,10 @@
 window.onload = init;
 
 function init() {
-	const topicWidget = document.querySelector('#topicWidget');
-	checkIfWideScreen();
+	const topicsWidget = document.querySelector('#topicsWidget');
+	const articlesHeading = document.querySelector('#articlesHeading');
+	toggleTopicsMenu();
+	activateTopicsButtons();
 }
 
 // Always open topic widget on desktop and disable toggle
@@ -10,18 +12,18 @@ function init() {
 var isWideScreen = window.matchMedia("(min-width: 37.5rem)");
 var toggleState;
 
-function checkIfWideScreen() {
+function toggleTopicsMenu() {
 	if (isWideScreen.matches) {
-		saveToggleState(topicWidget);
-		disableWidget(topicWidget);
-		openWidget(topicWidget);
+		saveToggleState(topicsWidget);
+		disableWidget(topicsWidget);
+		openWidget(topicsWidget);
 	}
 	else {
-		loadToggleState(topicWidget);
-		enableWidget(topicWidget);
+		loadToggleState(topicsWidget);
+		enableWidget(topicsWidget);
 	}
 }
-isWideScreen.addListener(checkIfWideScreen);
+isWideScreen.addListener(toggleTopicsMenu);
 
 function saveToggleState(id) {
 	if (id.hasAttribute("open")) {
@@ -33,7 +35,7 @@ function saveToggleState(id) {
 }
 
 function loadToggleState(id) {
-	if (toggleState == "isClosed") {
+	if (toggleState === "isClosed") {
 		id.removeAttribute("open");
 	}
 }
@@ -44,8 +46,8 @@ function disableWidget(id) {
 	id.getElementsByTagName("summary")[0].setAttribute("tabindex","-1");
  }
 
-function preventClick(evt) {
-	evt.preventDefault();
+function preventClick(tgt) {
+	tgt.preventDefault();
 }
 
 function enableWidget(id) {
@@ -54,6 +56,37 @@ function enableWidget(id) {
 	id.getElementsByTagName("summary")[0].removeAttribute("tabindex");
  }
 
- function openWidget(id) {
+function openWidget(id) {
 	id.open = true;
+}
+
+function closeWidget(id) {
+	id.open = false;
+}
+
+
+// Change title and load relevant articles upon topic button press
+
+var currentTopic = "Popular Articles";
+
+function activateTopicsButtons() {
+	topicsWidget.addEventListener('click', function (evt) {
+		if (evt.target.tagName === 'BUTTON') {
+			currentTopic = evt.target.textContent;
+			if(!isWideScreen.matches) {
+				closeWidget(topicsWidget);
+				document.location.replace('#articlesHeading');
+			}
+			topicsWidget.querySelectorAll('button').forEach(element => {
+				element.removeAttribute("disabled");
+			});
+			evt.target.setAttribute("disabled","");
+			articlesHeading.innerHTML = currentTopic;
+			loadArticles();
+		}
+	})
+}
+
+function loadArticles(){
+	console.log('todo load articles');
 }
