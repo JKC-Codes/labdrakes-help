5815af025768dca6efb23cea878298fa49ef85fb
HalcyonHush <34430187+HalcyonHush@users.noreply.github.com>
2018-02-03 22:34:32 +0000

    Added related articles display

--- js/article.js
+++ js/article.js
@@ -1,10 +1,63 @@
 window.onload = init;
 
 function init() {
+	// Update page tab's title
 	let pageTitle = document.querySelector('head > title');
 	let pageHeading = document.querySelector('#articleTitle');
 	pageTitle.innerHTML = pageHeading.textContent + pageTitle.innerHTML;
+
+	// Load related articles
+	loadArticles();
 }
 
+function loadArticles() {
+    var queryURL = "../js/articleslist.json";
+	fetch(queryURL)
 
-// Need to load relevant related articles
+	.then(function (response) {
+		return response.json();
+	})
+
+	.then(function (list) {
+		removeCurrentPage(list);
+	})
+
+	.catch(function (error) {
+		console.log('Error during fetch: ' + error.message);
+	});
+}
+
+function removeCurrentPage(articles) {
+	var url = document.location.pathname;
+	var currentPage = url.slice(url.lastIndexOf('/') + 1);
+	var currentTopic = 'unknown';
+
+	// Get page's topic and then remove current article from array
+	for(i = 0; i < articles.length; i++) {
+		if(articles[i].url + '.html' === currentPage) {
+			currentTopic = articles[i].topic;
+			articles.splice([i],1);
+			i--;
+		}
+	}
+
+	displayArticles(articles, currentTopic);
+}
+
+function displayArticles(list, topic) {
+	var filteredList = list.filter(article => article.topic === topic);
+
+	var sortedList = filteredList.sort(function(a, b) {
+		return b.hits - a.hits;
+	});
+
+	// Display articles
+	var displayArea = document.querySelector('#relatedArticlesList');
+	displayArea.innerHTML = '';
+
+	for (i = 0; i < 5; i++) {
+		let li = document.createElement('li');
+		li.innerHTML = '<a href="' + sortedList[i].url + '.html">' + sortedList[i].title + '</a>';
+		displayArea.appendChild(li);
+	}
+}
