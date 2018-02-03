7f449225e0c648cff53d585d3e0a573e2906e644
HalcyonHush <34430187+HalcyonHush@users.noreply.github.com>
2018-02-03 12:23:59 +0000

    Changed external links to #

--- js/footer.js
+++ js/footer.js
@@ -17,32 +17,32 @@ document.querySelector('footer').innerHTML = `
 			</a>
 		</li>
 		<li class="essa">
-			<a href="https://github.com/HalcyonHush/labdrakes-help" rel="noopener" class="icon essa">
+			<a href="#" rel="noopener" class="icon essa">
 				<span class="a11y">ESSA Sports Betting Integrity</span>
 			</a>
 		</li>
 		<li class="therapy">
-			<a href="https://github.com/HalcyonHush/labdrakes-help" rel="noopener" class="icon therapy">
+			<a href="#" rel="noopener" class="icon therapy">
 				<span class="a11y">Gambling Therapy</span>
 			</a>
 		</li>
 		<li class="gamcare">
-			<a href="https://github.com/HalcyonHush/labdrakes-help" rel="noopener" class="icon gamcare">
+			<a href="#" rel="noopener" class="icon gamcare">
 				<span class="a11y">Gamcare</span>
 			</a>
 		</li>
 		<li class="gib">
-			<a href="https://github.com/HalcyonHush/labdrakes-help" rel="noopener" class="icon gib">
+			<a href="#" rel="noopener" class="icon gib">
 				<span class="a11y">Government of Gibraltar</span>
 			</a>
 		</li>
 		<li class="commission">
-			<a href="https://github.com/HalcyonHush/labdrakes-help" rel="noopener" class="icon commission">
+			<a href="#" rel="noopener" class="icon commission">
 				<span class="a11y">Gambling Commission</span>
 			</a>
 		</li>
 		<li class="trust">
-			<a href="https://github.com/HalcyonHush/labdrakes-help" rel="noopener" class="icon trust">
+			<a href="#" rel="noopener" class="icon trust">
 				<span class="a11y">Responsible Gambling Trust</span>
 			</a>
 		</li>
@@ -52,9 +52,9 @@ document.querySelector('footer').innerHTML = `
 <section class="about">
 	<h2 class="h4">About Labdrakes</h2>
 	<ul class="no-bullets">
-		<li><a href="https://github.com/HalcyonHush/labdrakes-help" rel="noopener">About Us</a></li>
-		<li><a href="https://github.com/HalcyonHush/labdrakes-help" rel="noopener">Affiliates</a></li>
-		<li><a href="https://github.com/HalcyonHush/labdrakes-help" rel="noopener">Investor Centre</a></li>
+		<li><a href="#" rel="noopener">About Us</a></li>
+		<li><a href="#" rel="noopener">Affiliates</a></li>
+		<li><a href="#" rel="noopener">Investor Centre</a></li>
 		<li><a href="../pages/restricted-territories.html">Restricted Territories</a></li>
 	</ul>
 </section>
@@ -62,10 +62,10 @@ document.querySelector('footer').innerHTML = `
 <section class="quick-links">
 	<h2 class="h4">Quick Links</h2>
 	<ul class="no-bullets">
-		<li><a href="https://github.com/HalcyonHush/labdrakes-help" rel="noopener">Labdrakes.com</a></li>
-		<li><a href="https://github.com/HalcyonHush/labdrakes-help" rel="noopener">Shop Locator</a></li>
+		<li><a href="#" rel="noopener">Labdrakes.com</a></li>
+		<li><a href="#" rel="noopener">Shop Locator</a></li>
 		<li><a href="../pages/other-ways-to-bet.html">Other Ways To Bet</a></li>
-		<li><a href="https://github.com/HalcyonHush/labdrakes-help" rel="noopener">The Grid</a></li>
+		<li><a href="#" rel="noopener">The Grid</a></li>
 	</ul>
 </section>
 
@@ -73,17 +73,17 @@ document.querySelector('footer').innerHTML = `
 	<h2 class="a11y">Social Media</h2>
 	<ul class="no-bullets row">
 		<li>
-			<a href="https://github.com/HalcyonHush/labdrakes-help" rel="noopener" class="icon facebook">
+			<a href="#" rel="noopener" class="icon facebook">
 				<span class="a11y">Facebook</span>
 			</a>
 		</li>
 		<li>
-			<a href="https://github.com/HalcyonHush/labdrakes-help" rel="noopener" class="icon twitter">
+			<a href="#" rel="noopener" class="icon twitter">
 				<span class="a11y">Twitter</span>
 			</a>
 		</li>
 		<li>
-			<a href="https://github.com/HalcyonHush/labdrakes-help" rel="noopener" class="icon youtube">
+			<a href="#" rel="noopener" class="icon youtube">
 				<span class="a11y">YouTube</span>
 			</a>
 		</li>
@@ -92,7 +92,7 @@ document.querySelector('footer').innerHTML = `
 
 <section class="legal">
 	<h2 class="a11y">Legal</h2>
-	<p><small>Labdrakes Betting &amp; Gaming Ltd. is licensed (<a href="https://github.com/HalcyonHush/labdrakes-help" rel="noopener">licence no. 0000</a>) and regulated by the British Gambling Commission for persons gambling in Great Britain and LM International Limited, Suites 0-0, 0th Floor, Europort, Gibraltar are licensed (RGL Nos. 000, 000) by the Government of Gibraltar and regulated by the Gibraltar Gambling Commissioner.</small></p>
+	<p><small>Labdrakes Betting &amp; Gaming Ltd. is licensed (<a href="#" rel="noopener">licence no. 0000</a>) and regulated by the British Gambling Commission for persons gambling in Great Britain and LM International Limited, Suites 0-0, 0th Floor, Europort, Gibraltar are licensed (RGL Nos. 000, 000) by the Government of Gibraltar and regulated by the Gibraltar Gambling Commissioner.</small></p>
 	<p class="copyright"><small>Copyright &copy; 2018 Labdrakes</small></p>
 </section>
 `;
