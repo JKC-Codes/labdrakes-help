eba908f5861942715b0544dba057c18a122a6438
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2018-02-19 14:20:17 +0000

    Added hide sections function

--- js/contact.js
+++ js/contact.js
@@ -1,8 +1,30 @@
 /*
-[] hide inactive sections
+[x] hide inactive sections
 [] validate form before next step
 [] save progress
+[] change page
 [] get related articles
 [] disable form submission
 */
 
+document.addEventListener('DOMContentLoaded', init => {
+	form = document.querySelector('#contact-form');
+	currentPage = getComputedStyle(form).getPropertyValue('--current-step');
+	allPages = form.querySelectorAll('fieldset');
+
+	hideInactiveSections();
+});
+
+var form = document.querySelector('#contact-form');
+var currentPage = getComputedStyle(form).getPropertyValue('--current-step');
+var allPages = form.querySelectorAll('fieldset');
+
+function hideInactiveSections() {
+	for (let i = 0; i < allPages.length; i++) {
+		if (i == currentPage) {
+			allPages[i].removeAttribute('data-hidden');
+		} else {
+			allPages[i].setAttribute('data-hidden', 'true');
+		}
+	}
+}
