16076f8288db210507be859111ef5f6559cf9c5f
JKC-Codes <34430187+JKC-Codes@users.noreply.github.com>
2018-02-19 16:11:06 +0000

    Completed initial change steps logic

--- js/contact.js
+++ js/contact.js
@@ -2,29 +2,56 @@
 [x] hide inactive sections
 [] validate form before next step
 [] save progress
-[] change page
+[x] change page
 [] get related articles
 [] disable form submission
 */
 
 document.addEventListener('DOMContentLoaded', init => {
 	form = document.querySelector('#contact-form');
-	currentPage = getComputedStyle(form).getPropertyValue('--current-step');
-	allPages = form.querySelectorAll('fieldset');
 
-	hideInactiveSections();
-});
+	hideInactiveSteps();
+	activateFormButtons();
+}, {once:true});
 
-var form = document.querySelector('#contact-form');
-var currentPage = getComputedStyle(form).getPropertyValue('--current-step');
-var allPages = form.querySelectorAll('fieldset');
+var form;
+var currentStepIndex = 0;
 
-function hideInactiveSections() {
-	for (let i = 0; i < allPages.length; i++) {
-		if (i == currentPage) {
-			allPages[i].removeAttribute('data-hidden');
+function hideInactiveSteps() {
+	let allSteps = form.querySelectorAll('fieldset');
+	let currentStep = allSteps[currentStepIndex];
+
+	allSteps.forEach( step => {
+		if (step === currentStep) {
+			step.removeAttribute('data-hidden')
 		} else {
-			allPages[i].setAttribute('data-hidden', 'true');
+			step.setAttribute('data-hidden', 'true')
 		}
-	}
+	})
+}
+
+function activateFormButtons() {
+	let emailSection = document.querySelector('.email')
+
+	emailSection.addEventListener('click', evt => {
+		if (evt.target.matches('a')) {
+			if (evt.target.className === 'button') {
+				nextStep();
+			} else if (evt.target.className === 'secondary-nav') {
+				previousStep();
+			}
+		}
+	})
+}
+
+function nextStep() {
+	currentStepIndex += 1;
+	hideInactiveSteps()
+	form.style.setProperty('--current-step', currentStepIndex);
+}
+
+function previousStep() {
+	currentStepIndex -= 1;
+	hideInactiveSteps()
+	form.style.setProperty('--current-step', currentStepIndex);
 }
